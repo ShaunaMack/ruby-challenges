3fbba0100be52e78537109ff820fc41905cfe491
Shauna Mack <shaunakatemack@gmail.com>
2019-11-08 14:45:45 +1000

    completed 08_count_letters

--- 08_count_letters.rb
+++ 08_count_letters.rb
@@ -1,17 +1,62 @@
-# Count Letters
+# # Count Letters
 
-# Write a method that will take a string, keep count
-# of each letter and return the totals as a hash.
+# # Write a method that will take a string, keep count
+# # of each letter and return the totals as a hash.
 
-# Example:
-# count_letters("hello") should return {"h"=>1, "e"=>1, "l"=>2, "o"=>1}
-# count_letters("mississippi") should return {"m"=>1, "i"=>4, "s"=>4, "p"=>2}
+# # Example:
+# # count_letters("hello") should return {"h"=>1, "e"=>1, "l"=>2, "o"=>1}
+# # count_letters("mississippi") should return {"m"=>1, "i"=>4, "s"=>4, "p"=>2}
 
-# Check your solution by running the tests:
-# ruby tests/08_count_letters_test.rb
+# # Check your solution by running the tests:
+# # ruby tests/08_count_letters_test.rb
 
-def count_letters (string)
-  result = {} # You'll need an empty hash to get started!
+# #Array answer
+# def count_letters(words)
 
-  return result # return the hash
-end
+# words = words.split("")
+
+# frequencies = Hash.new(0)
+# words.each {|letter| frequencies[letter] += 1}
+
+# frequencies = frequencies.sort_by {|letter, count| count}
+# frequencies.reverse!
+
+# print frequencies
+
+# end
+# count_letters("mississippi")
+
+# #Hash version
+# require 'colorize'
+
+# puts "This is blue".colorize(:blue)
+
+# def count_letters(string)
+#     result = Hash.new(0)
+#     puts result
+#     string.each_char {|letter| 
+#         next unless letter =~/\w/
+#         result[letter] += 1
+#     }
+#     puts result
+# end
+
+#count_letters("mississippi")
+
+def count_letters(string)
+    result = {}
+    string.each_char do |letter|
+        if result[letter]
+            result[letter] += 1
+        else
+            result[letter] = 1
+        end
+    end  
+    
+    result.sort_by {|letter, count| count}
+    p result
+  end
+  
+  count_letters("mississippi")
+
+
