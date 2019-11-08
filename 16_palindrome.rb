8f362bdf86af046b38b7e6f8aafc8f21d35a252a
Shauna Mack <shaunakatemack@gmail.com>
2019-11-09 09:56:00 +1000

    Completed 16_palindrome

--- 16_palindrome.rb
+++ 16_palindrome.rb
@@ -20,5 +20,49 @@
 # ruby tests/16_palindrome_test.rb
 
 def palindrome (string)
-  # Your code here
+  i = 0
+  pal = true
+  until pal == false || i == string.length/2
+    # index starts at 0 so need to take 1 from length
+    if string[i] == string[string.length - 1 - i]
+      i += 1
+    else
+      pal = false
+      break # used break to make it clear that no further check required if false
+    end
+  end
+  # puts the boolean variable rather than 2 strings during loop
+  puts pal
 end
+
+palindrome("hello")
+palindrome("racecar")
+
+
+# attemting to make code nicer
+# def v2_palindrome(string)
+#   pal = true
+#   for (i = 0; i++; i < string.length/2) 
+#     if string[i] != string[string.length - 1 -i]
+#       pal = false
+#       break
+#     end
+#   end
+#   puts pal
+# end
+
+# v2_palindrome("anna")
+# v2_palindrome("meow")
+
+# after finding the method .reverse =)
+def v3_palindrome(string)
+  backward = string.reverse
+  if backward == string
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+v3_palindrome("anna")
+v3_palindrome("meow")
